da0e9c651636fc0ece8a6c443479c8fa9029199b
Gracey789 <Gmwende487@gmail.com>
2024-03-30 16:41:31 +0300

    feat: modified index.js

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCase);
 }
+
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+
+console.log(titleCased());
